5f0650f70df526e72346ee5096be3609c17b3afe
Rakesh <rakeshkumar8900@gmail.com>
2018-06-21 12:31:58 +0530

    static build controls

--- src/components/burger/burger.js
+++ src/components/burger/burger.js
@@ -19,10 +19,14 @@ let transformedingredients = Object.keys( props.ingredients )
         .map( igKey => {
             return [...Array( props.ingredients[igKey] )].map( ( _, i ) => {
                 return <BurgerIngredient key={igKey + i} type={igKey} />;
-            } );
-        } )
-        ;
-        console.log(transformedingredients);
+            });
+        }).reduce( (accu,curr) =>{
+          return accu.concat(curr);
+        },[]);
+                
+        if(transformedingredients.length === 0){
+          transformedingredients = <p>Please start adding ingredients.</p>;
+        }
    return (
       <div className={Classes.Burger}>
             <BurgerIngredient type="bread-top" />
--- src/containers/burgerBuilder/burgerBuilder.js
+++ src/containers/burgerBuilder/burgerBuilder.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import Aux from '../../hoc/aux';
-import Burger from '../../components/burger/burger'
+import Burger from '../../components/burger/burger';
+import BuildControl from '../../components/burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component{
   state = {
     ingredients: {
-      salad: 1,
-      bacon: 1,
-      cheese: 2,
-      meat: 2
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
     }
   }
 
@@ -16,7 +17,7 @@ class BurgerBuilder extends Component{
        return (
          <Aux>
            <Burger ingredients={this.state.ingredients}/>
-           <div>build Controls</div>
+           <BuildControl />
          </Aux>
        );
   }
